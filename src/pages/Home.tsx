7979e90ab2370b98ac0fae3d04dbf92e87f2ece3
Cleisson <cleissonconstc@gmail.com>
2021-06-24 13:38:25 -0300

    Adding routes, context and signIn with Google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,23 @@
+import { useHistory } from 'react-router-dom'
+import { Button } from '../components/Button'
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
-import { Button } from '../components/Button'
 import '../styles/auth.scss'
+import { useAuth } from '../hooks/useAuth';
 
-export function Home() {
+export function Home() { 
+  const history = useHistory();
+  const { user, signInWithGoogle} = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push('/rooms/new')
+  } 
+  
   return (
     <div id="page-auth">
       <aside>
@@ -15,7 +28,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Ícone Google" />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 import '../styles/auth.scss';
 
 export function NewRoom() {
+  const { user } = useAuth();
+
   return (
     <div id="page-auth">
       <aside>
@@ -14,6 +19,7 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
+          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
           <form>
             <input
@@ -25,7 +31,7 @@ export function NewRoom() {
             </Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#page-auth">clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
