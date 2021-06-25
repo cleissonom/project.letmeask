409caa2354f9b6d423baf4eb2bae7b2d1f518c02
Cleisson <cleissonconstc@gmail.com>
2021-06-24 21:28:49 -0300

    :tada: using relatime database adn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,16 @@ import { Button } from '../components/Button'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import googleIcon from '../assets/images/google-icon.svg';
 import '../styles/auth.scss'
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home() { 
   const history = useHistory();
-  const { user, signInWithGoogle} = useAuth();
+  const { user, signInWithGoogle } = useAuth();
+  const [ roomCode, setRoomCode ] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
@@ -17,6 +20,23 @@ export function Home() {
     }
     history.push('/rooms/new')
   } 
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.')
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`)
+  }
   
   return (
     <div id="page-auth">
@@ -29,14 +49,16 @@ export function Home() {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Ícone Google" />
+            <img src={googleIcon} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala" 
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               <img src="" alt="" />
